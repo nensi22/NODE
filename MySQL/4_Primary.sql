5aef51fa780ff8779cb694ab9ffb3865ba54d2bd
nensi22 <nensikumbhani2212@gmail.com>
2022-10-12 18:36:45 +0530

    Primary

---
+++ MySQL/4_Primary.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Categories(
+    CategoryId INT ,
+    CategoryName VARCHAR(100) ,
+    CONSTRAINT pk_Category PRIMARY KEY(CategoryId)
+);
+insert into Categories values(10001,'Electronics');
+insert into Categories values(10002,'Music');
+insert into Categories values(10002,'Music');
+
+
+QUERY
+----------
+--befor insertion
+    use skilldemo;
+    show databases;
+    show tables;
+    source c:\Users\RPCC\Desktop\Node\MySQL\4_Primary.sql
+    select * from Categories;
+--after insertion
+    source c:\Users\RPCC\Desktop\Node\MySQL\4_Primary.sql
+    select * from Categories;
--- MySQL/Queries.sql
+++ MySQL/Queries.sql
@@ -1,42 +1,4 @@
- open mysql shell => cli => Command Line Interface
-
-1.\sql											[\\switching to sql mode]
-2.\connect root@localhost:3309					[\\your ac]
-3.admin@123									[\\password]
-4.Y/N											[your pc work (Y) and other pc work (N)]
-5.show databases;								[pc in show all databases list]	
-6.create database name;							[create  new databases]
-7.show databases;								[pc in show all databases list]
-8.use databasesname;							[use your select databases]
-9.show tables;									[pc in show all tables list]
-10.create table table_name(colname datatype, . , . , . , .);	[create new table and columns]
-11.insert into table_name values(. , . , .),(. , . , .),(. , . , .);	[insert data in table]
-12.select * from table_name;						[show table]
-13.describe table_name;							[structur table]
-14.\quit										[exit sql]
-
-
-
-
- use skilldemo
- select * from table_name;
- select col1,col2 from table_name;
- select *, col from table_name;
- select name 'alias_name' from table_name;     --valid
- select name as 'alias_name' from table_name;    --valid
- desc table_name;     or      describe  table_name;
-
-WHERE CLUESE
- select name from client_master where name='Ivan';
-
- select name,city from client_master where baldue<1000;
-
- select name,city from client_master where baldue>1000;
-
- select name,city from client_master where baldue>1000 and city!='Bombay';
-
-
-
+DATE:11/10/2022
 * EXERCISE ON RETRIEVING RECORDS FROM A TABLE
     --[A] Find out the names of all the clients.
         select name from client_master;  
@@ -51,7 +13,6 @@ WHERE CLUESE
     --[F] Find the names of salesman who have a salary equal to Rs.3000.
         select name from  salesman_master where salamt='3000';
 
-
 * EXERCISE ON UPDATING RECORDS IN A TABLE
     --[A] Change the city of ClientNo'C00005' to 'Bangalore'.
         update client_master set city='Banglore' where clientno='C00005';   
@@ -60,7 +21,6 @@ WHERE CLUESE
         update client_master set baldue='1000' where clientno='C00001';
         select * from client_master;
 
-
-**drop table client_master;
-**drop table salesman_master;
-**drop table product_master;
+*drop table client_master;
+*drop table salesman_master;
+*drop table product_master;
