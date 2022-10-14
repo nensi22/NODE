b64f009ded28f69bb8b1f1c6e3113ee16c7ef36a
nensi22 <nensikumbhani2212@gmail.com>
2022-10-14 14:14:16 +0530

    KEY

--- MySQL/1_SalesmanMaster.sql
+++ MySQL/1_SalesmanMaster.sql
@@ -1,20 +1,21 @@
 
-create table salesman_master(
-    salesman_no varchar(50) ,
-    salesmanname varchar(50) ,
-    address varchar(50) ,
-    city  varchar(50) ,
-    pincode numeric(38) ,
-    state varchar(50) ,
-    salamt numeric(50) ,
-    tgttoget numeric(50) ,
-    ytdsale numeric(50) ,
-    remark varchar(50)
-);
-insert into salesman_master values('S00001','Kiran','A/14 worli','Bombay',400002,'Mah',3000,100,50,'Good');
-insert into salesman_master values('S00002','Manish','65,nariman','Bombay',400001,'Mah',3000,200,100,'Good');
-insert into salesman_master values('S00003','Ravi','p-7 Bandra','Bombay',400032,'Mah',3000,200,100,'Good');
-insert into salesman_master values('S00004','Ashish','A/5 Juhu','Bombay',400044,'Mah',3500,200,150,'Good');
-select * from salesman_master;
+-- create table SALESMAN_MASTER(
+--     salesman_no varchar(50) ,
+--     salesmanname varchar(50) ,
+--     address varchar(50) ,
+--     city  varchar(50) ,
+--     pincode numeric(38) ,
+--     state varchar(50) ,
+--     salamt numeric(50) ,
+--     tgttoget numeric(50) ,
+--     ytdsale numeric(50) ,
+--     remark varchar(50),
+--     CONSTRAINT pk_Category PRIMARY KEY(salesman_no)
+
+-- );
+insert into SALESMAN_MASTER values('S00001','Kiran','A/14 worli','Bombay',400002,'Mah',3000,100,50,'Good');
+insert into SALESMAN_MASTER values('S00002','Manish','65,nariman','Bombay',400001,'Mah',3000,200,100,'Good');
+insert into SALESMAN_MASTER values('S00003','Ravi','p-7 Bandra','Bombay',400032,'Mah',3000,200,100,'Good');
+insert into SALESMAN_MASTER values('S00004','Ashish','A/5 Juhu','Bombay',400044,'Mah',3500,200,150,'Good');
 
 
