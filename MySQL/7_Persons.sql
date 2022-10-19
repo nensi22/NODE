1f329fcfd4352909e7943529a2b7ca62b143c68d
nensi22 <nensikumbhani2212@gmail.com>
2022-10-19 12:36:55 +0530

    fs commite

---
+++ MySQL/7_Persons.sql
@@ -0,0 +1,15 @@
+-- CREATE TABLE persons(
+--     ID int ,
+--     FirstNAME varchar(50),
+--     LastName varchar(50),
+--     Age INT,
+--     City varchar(50),
+--     CONSTRAINT CHK_Person CHECK  (city='Amreli')
+-- );
+
+INSERT INTO persons VALUES(1,"Kuldip","Bhuva",23,"Amreli");
+INSERT INTO persons VALUES(2,"Keyur","Padmani",23,"Surat");
+INSERT INTO persons VALUES(3,"Vrushabh","Satasiya",24,"Surat");
+INSERT INTO persons VALUES(4,"Janki","Thummar",22,"Bhavanagar");
+INSERT INTO persons VALUES(5,"Vivek","Kachhadiya",25,"Bhavanagar");
+INSERT INTO persons VALUES(6,"Dharmesh","Kathiriya",24,"Amreli");
