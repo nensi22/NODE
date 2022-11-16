5ec28511a6404abdb3adc7d67e69415b17653711
nensi22 <nensikumbhani2212@gmail.com>
2022-11-16 14:12:51 +0530

    operators

--- JavaScript/FirstJS.js
+++ JavaScript/FirstJS.js
@@ -1,5 +1,5 @@
 // for (var i = 1; i <= 10; i++) {
-//     console.log("Hello Vrushabh");
+//     console.log("Hello ");
 //     if (i < 10) {
 //         console.log("In for loop");
 //     }
@@ -7,6 +7,7 @@
 //         console.log("Exit for loop");
 //     }
 // }
+
 
 // var n = 5;
 // var string = "";
@@ -20,3 +21,92 @@
 // }
 // console.log(string);
 
+//output:-
+
+        // *****
+        // *****
+        // *****
+        // *****
+        // *****
+
+
+// var n = 5;
+// var string = "";
+
+// for (var i = 0; i < n; i++) {
+//     for (let j = 0; j < i; j++) {
+//         string += "*";
+//     }
+
+//     string += "\n";
+// }
+// console.log(string);
+
+//output:-
+
+        // *
+        // **
+        // ***
+        // ****
+
+
+// var n = 5;
+// var string = "";
+
+// for (var i = 0; i < n; i++) {
+//     for (var j = 0; j < n; j++) {
+//         if (i === 0 || i === n - 1) {
+//             string += "*";
+//         }
+//         else {
+//             if (j === 0 || j === n - 1) {
+//                 string += "*";
+//             }
+//             else {
+//                 string += " ";
+//             }
+//         }
+//     }
+
+//     string += "\n";
+// }
+
+// console.log(string);
+
+// output:-
+
+        // *****
+        // *   *
+        // *   *
+        // *   *
+        // *****
+
+
+// function sum(a, b) {
+//     return a + b;
+// }
+// function sub(a, b) {
+//     return a - b;
+// }
+// function mul(a, b) {
+//     return a * b;
+// }
+// function div(a, b) {
+//     return a / b;
+// }
+// function mod(a, b) {
+//     return a % b;
+// }
+// console.log("a + b =", sum(20, 10));
+// console.log("a - b =", sub(20, 10));
+// console.log("a * b =", mul(20, 10));
+// console.log("a / b =", div(20, 10));
+// console.log("a % b =", mod(20, 10));
+
+// output:-
+
+        // a + b = 30
+        // a - b = 10
+        // a * b = 200
+        // a / b = 2
+        // a % b = 0
--- JavaScript/Operators.js
+++ JavaScript/Operators.js
@@ -1,41 +1,124 @@
-// console.log(10+20);   //:-30
-// console.log(10+"20");    //:-1020
-// console.log(10 + + "20");    //:-30
-// console.log(typeof +"20");   //:-number
-// console.log(typeof "20");    //:-string
-// console.log(10 + + "20" + 20);   //:-50
-// console.log(10 * "20");  //:-200
-// console.log(10 / "20");  //:-0.5
-// console.log(true+true);  //:-2
-// console.log(true+false); //:-1
-// console.log(true+1);     //:-2
-// console.log(true+10+20/5*false);     //:-11
-// console.log(true/false);         //:-Infinity
-// console.log(20*5/false);     //:-Infinity
-// console.log(10+20/5*4-1);        //:-25
+//:-OPERATORS
+
+console.log(10+20);   
+//                :-30
+
+console.log(10+"20");    
+//                  :-1020
+
+console.log(10 + + "20");   
+//                      :-30
+
+console.log(typeof +"20");   
+//                      :-number
+
+console.log(typeof "20");    
+//                      :-string
+
+console.log(10 + + "20" + 20);   
+//                          :-50
+
+console.log(10 * "20");  
+//                    :-200
+
+console.log(10 / "20");  
+//                    :-0.5
 
 
-// assignment operator             =        
-// comparision operator            ==
-// strictly comparision operator   ===
-// console.log(10 == 10);   //:-true
-// console.log(10 == "10");     //:-true
-// console.log(10 === 10);     //:-true
-// console.log(10 === "10");    //:-false
-// console.log(10 == "ten");    //:-false
-// console.log(0.1+0.2 == 0.3);     //:-false
-// console.log(0.1+0.2);    //:-0.30000000000000004
-// console.log(0.2+0.2 == 0.4);     //:-true
-// console.log(0.1+0.3 == 0.4);     //:-true
-// console.log(typeof "skill");     //:-string
-// console.log(typeof 10);      //:-number
-// console.log(typeof 10.1);        //:-number
-// console.log(typeof true);      //:-boolean
-// console.log(typeof undefined);       //:-undefined
-// console.log(typeof NaN);     //:-number
-// console.log(typeof null);    //:-object
-// console.log(typeof []);      //:-object
-// console.log(typeof {});      //:-object
-// console.log(typeof 100n);        //:-bigint
-// console.log(typeof function(){});        //:-function
-// console.log(typeof Symbol('A'));     //:-symbol
+// true=1        false=0
+console.log(true+true);  
+//                    :-2
+
+console.log(true+false); 
+//                      :-1
+
+console.log(True+1);     
+//                  True is not defined
+
+console.log(true+10+20/5*false);     
+//                             :-11
+
+console.log(true/false);         
+//                     :-Infinity
+
+console.log(20*5/false);     
+//                      :-Infinity
+
+console.log(10+20/5*4-1);        
+//                      :-25
+
+
+// Assignment  Operator    =  
+//                     [Assigns a value to a variable]  
+
+// Comparision Operator    ==
+//                     [Compar a value]
+
+// Strictly Comparision Operator   ===
+//                              [Compar a value and a datatype]
+
+// console.log(10 == 10);   
+//                   :-true
+
+console.log(10 == "10");     
+//                     :-true
+
+console.log(10 === 10);     
+//                     :-true
+
+console.log(10 === "10");    
+//                      :-false
+console.log(10 == "ten");    
+//                      :-false
+
+console.log(0.1+0.2 == 0.3);     
+//                         :-false
+
+console.log(0.1+0.2);    
+//                  :-0.30000000000000004
+
+console.log(0.2+0.2 == 0.4);     
+//                         :-true
+
+console.log(0.1+0.3 == 0.4);     
+//                         :-true
+
+
+//DATATYPE:-
+
+console.log(typeof "skill");     
+//                         :-string
+
+console.log(typeof 10);      
+//                    :-number
+
+console.log(typeof 10.1);        
+//                      :-number
+
+console.log(typeof true);      
+//                      :-boolean
+
+console.log(typeof undefined);       
+//                           :-undefined
+
+console.log(typeof NaN);     
+//                     :-number
+//          [NaN :- Not a Number]
+
+console.log(typeof null);    
+//                      :-object
+
+console.log(typeof []);      
+//                    :-object
+
+console.log(typeof {});      
+//                    :-object
+
+console.log(typeof 100n);        
+//                      :-bigint
+
+console.log(typeof function(){});        
+//                              :-function
+
+console.log(typeof Symbol('A'));     
+//                             :-symbol
