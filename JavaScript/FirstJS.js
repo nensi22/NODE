ffc391997849529b38d81abda11b99c7c03d7ce0
nensi22 <nensikumbhani2212@gmail.com>
2022-11-16 14:06:36 +0530

    operators

---
+++ JavaScript/FirstJS.js
@@ -0,0 +1,22 @@
+// for (var i = 1; i <= 10; i++) {
+//     console.log("Hello Vrushabh");
+//     if (i < 10) {
+//         console.log("In for loop");
+//     }
+//     else {
+//         console.log("Exit for loop");
+//     }
+// }
+
+// var n = 5;
+// var string = "";
+
+// for (var i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//         string += "*";
+//     }
+
+//     string += "\n";
+// }
+// console.log(string);
+
