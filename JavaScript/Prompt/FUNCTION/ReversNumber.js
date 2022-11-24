b916a888e6d38a8003f3e8a25cc528e83b73a5fa
nensi22 <nensikumbhani2212@gmail.com>
2022-11-24 10:53:49 +0530

    fun_task

---
+++ JavaScript/Prompt/FUNCTION/ReversNumber.js
@@ -0,0 +1,34 @@
+/*  1. Write a JS function that reverse a number.
+Example x = 32243;
+Expected Output : 34223 */
+
+// Named function
+// let rev=0,rem=0;
+// function reversNumber(x){
+//     while(x!=0)
+//     {
+//       rem=x%10;
+//       rev=(rev*10)+rem;
+//       x=Math.floor(x/10);
+//     }
+//     console.log(`Revers a Number is : ${rev}`);
+// }
+
+// reversNumber(32243);
+
+
+// Anonymous function
+
+// let rev=0,rem=0;
+// let reversNumber=(x)=>{
+//     while(x!=0)
+//         {
+//           rem=x%10;
+//           rev=(rev*10)+rem;
+//           x=Math.floor(x/10);
+//         }
+//     console.log(`Revers a Number is : ${rev}`);
+// }
+//  reversNumber(32243);
+
+
